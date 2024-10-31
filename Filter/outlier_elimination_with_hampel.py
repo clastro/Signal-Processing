40aeea657d68a559d6b80311b1cf568689a637c1
Song S.H. <37033795+clastro@users.noreply.github.com>
2024-11-01 00:07:01 +0900

    Delete Filter/outlier_elimination_with_hampel.py

--- Filter/outlier_elimination_with_hampel.py
+++
@@ -1,6 +0,0 @@
-from hampel import hampel
-outlier_indices = hampel(pd.Series(ecg_wave), window_size=5, n=3)
-wave_imputation = hampel(pd.Series(ecg_wave), window_size=5, n=3, imputation=True)
-pd.Series(ecg_wave).plot(style="k-")
-wave_imputation.plot(style="g-")
-plt.show()
